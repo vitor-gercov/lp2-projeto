dcfa35ae347e578e1724f0fb3ade44a20b6aa932
iamseki <chrisyuri_19@hotmail.com>
2020-09-02 23:22:11 -0300

    Adicionando camadas dao e service

---
+++ api/src/main/java/mtruck/api/daos/SampleDAO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mtruck.api.daos;
+
+/**
+ *
+ * @author GERU\christian.seki
+ */
+public class SampleDAO {
+    
+}
