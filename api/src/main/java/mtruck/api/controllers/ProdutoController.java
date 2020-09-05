21b8688c1acfc6dd544bf7760623eada3cf328bc
HigorReculiano <you@example.com>
2020-09-04 22:18:44 -0300

    Criacao DAO do Produtos

--- api/src/main/java/mtruck/api/controllers/ProdutoController.java
+++ api/src/main/java/mtruck/api/controllers/ProdutoController.java
@@ -5,10 +5,19 @@
  */
 package mtruck.api.controllers;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import mtruck.api.daos.ProdutoDAO;
 import mtruck.api.entities.Produto;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,42 +37,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/produto")
 @RestController
 public class ProdutoController {
-    
+
     @GetMapping
-    List<Produto> listar(){
-        
+    List<Produto> listar() throws FileNotFoundException, UnsupportedEncodingException {
+
         Produto p1 = new Produto(1, "Roupa", "Playboy", "M", "Blusa Gola Polo", "Preto", 200.2, 245.4, 200.2, 350.2, "Rua João Gomes", new Date());
-        
+
         Produto p2 = new Produto(2, "Roupa", "Playboy", "M", "Blusa Gola Polo", "Preto", 200.2, 245.4, 200.2, 350.2, "Rua João Gomes", new Date());
-        
+
         List<Produto> produtos = new ArrayList<Produto>();
-        
+
         produtos.add(p1);
         produtos.add(p2);
-        
+
+        ProdutoDAO produtoDAO = new ProdutoDAO();
+        produtoDAO.criaTxt(p1);
         return produtos;
     }
-    
+
     @ResponseStatus(code = HttpStatus.CREATED)
     @PostMapping
-    Produto cadastrar(@RequestBody Produto newProduto){
+    Produto cadastrar(@RequestBody Produto newProduto) {
         return newProduto;
     }
-    
+
     @GetMapping("/{id}")
-    Produto pesquisar(@PathVariable int id){
+    Produto pesquisar(@PathVariable int id) {
         return new Produto(1, "Roupa", "Playboy", "M", "Blusa Gola Polo", "Preto", 200.2, 245.4, 200.2, 350.2, "Rua João Gomes", new Date());
     }
-    
+
     @PatchMapping("/{id}")
-    Produto editar(@RequestBody Produto newProduto, @PathVariable int id){
+    Produto editar(@RequestBody Produto newProduto, @PathVariable int id) {
         newProduto.setId(id);
         return newProduto;
     }
-    
+
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
-    void deletar(@PathVariable int id){
-        
+    void deletar(@PathVariable int id) {
+
     }
 }
--- api/src/main/java/mtruck/api/daos/ProdutoDAO.java
+++ api/src/main/java/mtruck/api/daos/ProdutoDAO.java
@@ -5,11 +5,41 @@
  */
 package mtruck.api.daos;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import mtruck.api.entities.Produto;
+
 /**
  *
  * @author GERU\christian.seki
  */
 public class ProdutoDAO {
-    
-    
+
+    public void criaTxt(Produto produto) {
+        try {
+            File file = new File("estoque.txt");
+
+            if (file.exists()) {
+
+                FileWriter fw = new FileWriter(file.getName(), true);
+                BufferedWriter buffer = new BufferedWriter(fw);
+
+                PrintWriter pw = new PrintWriter(buffer);
+                pw.println(produto.getCategoria());
+                pw.close();
+            } else {
+                PrintWriter pw = new PrintWriter(new File("estoque.txt"));
+                pw.println(produto.getCategoria());
+                pw.close();
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+    }
+
 }
