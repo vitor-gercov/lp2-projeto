12fe45da09846cb236fda7bd31678520ffcc1a18
Renato Vieira <renato@getconnect.com.br>
2020-09-05 16:16:46 -0300

    Fazendo enum maquinEstado

--- lp2-projeto-login/src/frontend/Menu.java
+++ lp2-projeto-login/src/frontend/Menu.java
@@ -2,20 +2,31 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+
  */
 package frontend;
 
+import maquinaEstado.maquinaEstado;
+import maquinaEstado.enumEstado;
 /**
  *
  * @author Usuario
  */
 public class Menu {
 
+     public static maquinaEstado estadoConsole;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        estadoConsole = enumEstado.MENU.getEstadoMaquina();;
+        Boolean saida = false;
+        while (!saida) {
+            saida = estadoConsole.executa();
+        }
     }
     
 }
