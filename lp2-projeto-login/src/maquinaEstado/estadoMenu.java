0ff70d598f75301c9960d2e8082a1a6f638ffd35
Renato Vieira <renato@getconnect.com.br>
2020-09-05 16:39:47 -0300

    Fazendo menu inicial

--- lp2-projeto-login/src/maquinaEstado/estadoMenu.java
+++ lp2-projeto-login/src/maquinaEstado/estadoMenu.java
@@ -5,6 +5,9 @@
  */
 package maquinaEstado;
 
+import java.util.Scanner;
+import frontend.Menu;
+
 /**
  *
  * @author oseas
@@ -13,6 +16,34 @@ public class estadoMenu extends maquinaEstado{
      @Override
     public boolean
     executa(){
-        return false;
+         boolean sair = false;
+        System.out.println("Bem-vindo!");
+        System.out.println("Indique a opção desejada!");
+        System.out.println("1 - Cadastrar produto");
+        System.out.println("2 - Editar produto");
+        System.out.println("3 - Seleciona Produto");
+        System.out.println("4 - Excluir produto");
+        System.out.println("0 - Sair");
+        Scanner scan = new Scanner(System.in);
+        int opcao = scan.nextInt();
+        switch (opcao)
+        {
+            case 0:
+                sair = true;
+                break;
+            case 1:
+                Menu.estadoConsole = enumEstado.REGISTRA_PRODUTO.getEstadoMaquina();
+                break;
+            case 2:
+                Menu.estadoConsole = enumEstado.EDITA_PRODUTO.getEstadoMaquina();
+                break;
+            case 3:
+                Menu.estadoConsole = enumEstado.SELECIONA_PRODUTO.getEstadoMaquina();
+                break;
+            case 4:
+                Menu.estadoConsole = enumEstado.EXCLUI_PRODUTO.getEstadoMaquina();
+                break;
+        }
+        return sair;
     }
 }
